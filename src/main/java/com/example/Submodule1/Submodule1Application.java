5c95432f1d7fbb4692bac646e6e277ff59729ac1
DeveloperBilbao91 <jonandonicastelo@outlook.com>
2021-07-01 17:08:13 +0200

    First commit submodule 1

---
+++ src/main/java/com/example/Submodule1/Submodule1Application.java
@@ -0,0 +1,13 @@
+package com.example.Submodule1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Submodule1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Submodule1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Submodule1/Submodule1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Submodule1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Submodule1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
